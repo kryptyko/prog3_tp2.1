d764532fcaa10d27e0c8e8f8dc4654ca596e8e9d
kryptyko <kryptyko@gmail.com>
2024-06-10 18:25:03 -0300

    Funcionando completo ejercicio2, se agrego la class reservation con su getinfo y validateReservation

--- ejercicio2/reservations.js
+++ ejercicio2/reservations.js
@@ -11,8 +11,46 @@ class Customer {
     }
 }
 
+
+/*
+Definir una clase `Reservation` en el archivo `reservations.js` que permita representar una reserva.
+    - La clase debe tener las siguientes propiedades:
+        - `id`: Identificador de la reserva.
+        - `customer`: instancia de la clase `Customer` que realiza la reserva.
+        - `date`: Fecha y hora de la reserva.
+        - `guests`: Número de comensales de la reserva.
+    - La clase debe tener un constructor.
+    - Se debe implementar una propiedad computada `info` que retorne una cadena con la fecha y hora 
+    de la reserva, la información del cliente y el número de comensales.
+    - Se debe implementar un método estático `validateReservation` que reciba un objeto con la información 
+    de la reserva y retorne `true` si la información es válida y `false` en caso contrario. 
+    Si la fecha de la reserva es anterior a la fecha actual y la cantidad de comensales es menor o igual a 0,
+     la reserva no es válida.
+
+
+*/
 class Reservation {
-    //to do
+    //contructor para el objeto Reservation
+    constructor(id, customer, date, guests) {
+        this.id = id; 
+        this.customer = customer;
+        this.date = new Date(date); // Convertir la fecha a objeto Date
+        this.guests = guests;
+    }
+    //computada para obtener la información de la reserva
+    get info() {
+        return `
+        Fecha: ${this.date.toLocaleString()}
+        Cliente: ${this.customer.info}
+        Comensales: ${this.guests}
+        `; 
+    }
+    //metodo estatico para validar la reserva (verificar que la fecha elegida sea mayor a la fecha actual y que el numero de comensales sea mayor a 0)
+    static validateReservation(date, guests) {
+        const now = new Date();
+        const inputDate = new Date(date);
+        return inputDate.getTime() >= now.getTime() && guests > 0;
+      }
 }
 
 class Restaurant {
